d0cf5469ddc5b8c9ce732c93bda6936831398423
Duy Van <vanquangduy10@gmail.com>
2022-04-13 23:58:21 +0700

    3.18

--- base/views.py
+++ base/views.py
@@ -1,8 +1,12 @@
+from itertools import product
 from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
+
+from base.models import Product
 from .products import products
 from rest_framework.response import Response
+from .serializers import ProductSerializer
 
 # Create your views here.
 
@@ -12,13 +16,13 @@ def getRoutes(request):
 
 @api_view(['GET'])
 def getProducts(request):
-    return Response(products)
+    products=Product.objects.all()
+    serializers = ProductSerializer(products, many=True)
+    return Response(serializers.data)
 
 @api_view(['GET'])
 def getProduct(request, pk):
-    for i in products:
-        if i['_id'] == pk:
-            product = i
-            break
-    return Response(product)
+    product = Product.objects.get(_id=pk)
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
 
