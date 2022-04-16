3dcdcbc0006ea594c36bfe41e48293da8870508c
Duy Van <vanquangduy10@gmail.com>
2022-04-16 19:01:53 +0700

    6.35_protect_route

--- base/urls.py
+++ base/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('api/users/login', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/', views.getRoutes, name="routes"),
     path('api/products', views.getProducts, name="products"),
     path('api/product/<str:pk>', views.getProduct, name="product"),
--- base/views.py
+++ base/views.py
@@ -1,12 +1,37 @@
+from cmath import pi
+import pandas as pd
+import json
 from itertools import product
 from django.shortcuts import render
 from django.http import JsonResponse
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+
+df=pd.read_csv("D:/software/ecommerce/backend/base/testdata.csv")
+datadf = df.to_dict(orient='records')
+print("datadf", datadf)
+# print((datadf[0]))
+# json_string = json.dumps(datadf) 
+# print(json_string)
 
 from base.models import Product
 from .products import products
 from rest_framework.response import Response
 from .serializers import ProductSerializer
+
+# JWT
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+# END
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        data['username'] = self.user.username
+        data['email'] = self.user.email
+        return data
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
 
 # Create your views here.
 
@@ -15,10 +40,12 @@ def getRoutes(request):
     return JsonResponse('Hello from getRoutes', safe=False)
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def getProducts(request):
     products=Product.objects.all()
     serializers = ProductSerializer(products, many=True)
-    return Response(serializers.data)
+    return Response(datadf)
+    # return Response(serializers.data)
 
 @api_view(['GET'])
 def getProduct(request, pk):
